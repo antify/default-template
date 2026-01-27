ca1fb19aa573149f37874ecf033133dd6b04218c
max <maximilian@blue-panda.de>
2026-01-27 07:54:46 +0100

    fix: remove nuxt dirs (#21)

--- playground/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "default"
-declare module "../../../node_modules/.pnpm/nuxt@3.16.2_@parcel+watcher@2.5.6_@types+node@20.19.30_cac@6.7.14_db0@0.3.4_ioredis@5.9_bcb9828f51a29e7b0a9054c71d12fac0/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
